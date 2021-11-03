58446b26be44bedd88fbb97110642a032c044d22
tiniuspre <70323207+tiniuspre@users.noreply.github.com>
2021-11-03 08:51:52 +0100

    drone_rc

--- tello/drone.py
+++ tello/drone.py
@@ -1,15 +1,16 @@
 import socket
 import threading
-
+import pickle
 
 class Tello:
-    def __init__(self, tello_ip: str='192.168.10.1'):
+    def __init__(self, tello_ip: str='192.168.10.1', rc: bool=False):
         self.local_ip = ''
         self.local_port = 8889
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((self.local_ip, self.local_port))
 
+        self.rc = rc
         self.tello_ip = tello_ip
         self.tello_port = 8889
         self.tello_address = (self.tello_ip, self.tello_port)
@@ -21,3 +22,7 @@ class Tello:
 
     def command(self):
         self.send_command('command')
+
+    def command_rc(self, forward_back, left_right, up_down, yaw):
+        if rc:
+            self.send_command(f'rc {left_right} {forward_back} {up_down} {yaw}')
